6ca0155852ff1e3f6e0b1d8cc50b8ffaa2115803
BethsFaith <BethsFaith7@gmail.com>
2023-11-04 00:03:30 +0300

    Moved to new directory

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"RnpServer/config"
-	"RnpServer/db"
+	"RnpServer/internal/config"
+	"RnpServer/internal/db"
+	"RnpServer/internal/log"
+	"golang.org/x/exp/slog"
 	"os"
 )
 
 func main() {
 	os.Setenv("CONFIG_PATH", "config/local.yaml")
 	cfg := config.MustLoad()
+
+	logger := log.SetupLogger(cfg.Env)
+	logger = logger.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+	logger.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	logger.Debug("logger debug mode enabled")
 
 	dataBase := new(db.Common)
 
