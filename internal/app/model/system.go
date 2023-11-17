87011f85884d73c6b6a6ed447213e65c58d007fb
BethsFaith <BethsFaith7@gmail.com>
2023-11-17 22:42:32 +0300

    Added the system component model

--- internal/app/model/system.go
+++ internal/app/model/system.go
@@ -9,11 +9,26 @@ type System struct {
 	Icon string `json:"icon"`
 }
 
+// SystemComponent ...
+type SystemComponent struct {
+	SystemId    int `json:"system_id"`
+	ComponentId int `json:"component_id"`
+}
+
 // Validate ...
-func (sc *System) Validate() error {
+func (s *System) Validate() error {
+	return validation.ValidateStruct(
+		s,
+		validation.Field(&s.Name, validation.Required, validation.Length(6, 255)),
+		validation.Field(&s.Icon, validation.Length(1, 1024)),
+	)
+}
+
+// Validate ...
+func (sc *SystemComponent) Validate() error {
 	return validation.ValidateStruct(
 		sc,
-		validation.Field(&sc.Name, validation.Required, validation.Length(6, 255)),
-		validation.Field(&sc.Icon, validation.Length(1, 1024)),
+		validation.Field(&sc.SystemId, validation.Required, validation.Min(1)),
+		validation.Field(&sc.ComponentId, validation.Required, validation.Min(1)),
 	)
 }
--- internal/app/model/system_test.go
+++ internal/app/model/system_test.go
@@ -50,3 +50,55 @@ func TestSystem_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestSystemComponent_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		s       func() *model.SystemComponent
+		isValid bool
+	}{
+		{
+			name: "valid",
+			s: func() *model.SystemComponent {
+				s := &model.SystemComponent{
+					SystemId:    1,
+					ComponentId: 1,
+				}
+				return s
+			},
+			isValid: true,
+		},
+		{
+			name: "no valid system id",
+			s: func() *model.SystemComponent {
+				s := &model.SystemComponent{
+					SystemId:    -1,
+					ComponentId: 1,
+				}
+				return s
+			},
+			isValid: false,
+		},
+		{
+			name: "no valid component id",
+			s: func() *model.SystemComponent {
+				s := &model.SystemComponent{
+					SystemId:    1,
+					ComponentId: -1,
+				}
+				return s
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.s().Validate())
+			} else {
+				assert.Error(t, tc.s().Validate())
+			}
+		})
+	}
+}
